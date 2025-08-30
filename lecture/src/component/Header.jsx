184bcba00ea7d1ef11a342ef09a15d03a4200787
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2025-08-31 00:03:49 +0530

    day 1 on react

---
+++ lecture/src/component/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full p-4 bg-sky-500'>
+      <h1>Test code</h1>
+      <div>
+     <link > Home</link>
+     <link > About</link>
+     <link > Contact</link>
+    
+      </div>
+    </div>
+  )
+}
+
+export default Header
